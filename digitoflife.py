365b1144005915ad1547646203b1d49b269c80d1
evusr1 <127464885+evusr1@users.noreply.github.com>
2024-07-16 08:01:37 -0700

    Create digitoflife.py

---
+++ digitoflife.py
@@ -0,0 +1,15 @@
+# SkillsForAll
+# Python Essentials 2 - 2.4.9 LAB
+# The Digit of Life
+
+bday = input("What is your birthday date YYYYMMDD, YYYYDDMM, or MMDDYYYY?")
+
+
+while int(bday) > 9:
+    bday_sum = 0
+    for digit in bday:
+        bday_sum += int(digit)
+    
+    bday = str(bday_sum)
+
+print(bday)
